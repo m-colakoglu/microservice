6f85bbf9ef265b0c594278ff7787979ca125ec2a
Mustafa Çolakoğlu <mustafacolakoglu7@gmail.com>
2022-07-05 19:30:15 +0300

    basket microservices added

---
+++ src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,36 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _distributedCache;
+        public BasketRepository(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache?? throw new ArgumentException(nameof(distributedCache));
+        }
+
+        public async Task DeleteBasket(string username)
+        {
+             await _distributedCache.RemoveAsync(username);
+        }
+
+        public async Task<ShoppingCart> GetBasket(string username)
+        {
+            var basket=await _distributedCache.GetStringAsync(username);
+            if (string.IsNullOrEmpty(basket))
+                return null;
+
+            return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+        }
+
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart shoppingCart)
+        {
+            await _distributedCache.SetStringAsync(shoppingCart.UserName, JsonConvert.SerializeObject(shoppingCart));
+
+            return await GetBasket(shoppingCart.UserName); 
+        }
+    }
+}
