c415943a5641dace8a65b22b8a6c9273ab2ec332
Mustafa Çolakoğlu <mustafacolakoglu7@gmail.com>
2022-07-08 19:24:40 +0300

    discount grpc added

---
+++ src/Services/Discount/Discount.Gprc/Mapper/DiscountProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Discount.Gprc.Entities;
+using Discount.Gprc.Protos;
+
+namespace Discount.Gprc.Mapper
+{
+    public class DiscountProfile:Profile
+    {
+        public DiscountProfile()
+        {
+            CreateMap<Coupon, CouponModel>().ReverseMap();
+        }
+    }
+}
