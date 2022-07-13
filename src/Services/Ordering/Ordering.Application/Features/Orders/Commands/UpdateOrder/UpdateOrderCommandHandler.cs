fd7db5d4df579ba021e29bae012f4a4f36019f22
Mustafa Çolakoğlu <mustafacolakoglu7@gmail.com>
2022-07-13 17:17:16 +0300

    infra added

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommandHandler.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Commands.UpdateOrder
+{
+    internal class UpdateOrderCommandHandler : IRequestHandler<UpdateOrderCommand>
+    {
+        private readonly IOrderRepository _repository;
+        private readonly IMapper _mapper;
+        private readonly ILogger<UpdateOrderCommand> _logger;
+
+        public UpdateOrderCommandHandler(IOrderRepository repository, IMapper mapper, ILogger<UpdateOrderCommand> logger)
+        {
+            _repository = repository;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        public async Task<Unit> Handle(UpdateOrderCommand request, CancellationToken cancellationToken)
+        {
+            var orderToUpdate = await _repository.GetByIdAsync(request.Id);
+            if (orderToUpdate==null)
+            {
+                _logger.LogError("Order not exist on database");
+            }
+
+            _mapper.Map(request, orderToUpdate, typeof(UpdateOrderCommand), typeof(Order));
+            await _repository.UpdateAsync(orderToUpdate);
+
+            _logger.LogInformation("Update is succeed");
+
+            return Unit.Value;
+
+        }
+    }
+}
